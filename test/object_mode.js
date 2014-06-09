8691d80b14352e8171f56c934c38f67d17f21360
James Halliday <mail@substack.net>
2014-06-08 20:41:05 -0700

    opts.objectMode test

--- test/object_mode.js
+++ test/object_mode.js
@@ -28,3 +28,29 @@ test('falsey object mode', function (t) {
     
     function end () { t.ok('ended') }
 });
+
+test('falsey object mode option', function (t) {
+    var oldStream = new EventEmitter;
+    var wrapped = wrap(oldStream, { objectMode: true });
+    
+    var input = [ 5, 'a', false, 0, '', 'xyz', { x: 4 }, 7, [], 555 ];
+    var expected = input.slice();
+    t.plan(expected.length + 1);
+    
+    wrapped.pipe(through.obj(write, end));
+    
+    var iv = setInterval(function () {
+        if (input.length === 0) {
+            oldStream.emit('end');
+            clearInterval(iv);
+        }
+        else oldStream.emit('data', input.shift());
+    }, 5);
+    
+    function write (row, enc, next) {
+        t.deepEqual(row, expected.shift());
+        next();
+    }
+    
+    function end () { t.ok('ended') }
+});
